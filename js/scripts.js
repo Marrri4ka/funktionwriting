f20868fe8bf896d751549f4f1bfec4ef7235f661
Brendan Hellar and Mariia Stashuk <mariia@>
2019-03-25 15:12:32 -0700

    the issue of js not responding to click commands

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,58 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert(sentence);
+  });
+
+  $("p").click(function() {
+    alert(reverseString(upper));
+  });
 
 
-var sentence = prompt("Enter the sentence:");
-var result = function(){
-  return sentence.charAt(0).toUpperCase() + sentence.charAt(sentence.length-1).toUpperCase();
+});
 
-};
-alert(result());
+  var sentence = prompt("Enter the sentence:");
+  var result = function(){
+    return sentence.charAt(0).toUpperCase() + sentence.charAt(sentence.length-1).toUpperCase();
+
+  };
+  alert(result());
 
 
-var reverse = function result(){
-  return sentence.charAt(sentence.length-1).toUpperCase() + sentence.charAt(0).toUpperCase();
-};
+  var reverse = function result(){
+    return sentence.charAt(sentence.length-1).toUpperCase() + sentence.charAt(0).toUpperCase();
+  };
 
-alert(reverse());
+  alert(reverse());
+
+  var third = function(){
+    return sentence + reverse ();
+
+  };
+
+  alert(third());
+
+  var four = function(){
+    return sentence.charAt(Math.floor(sentence.length / 2))+third();
+
+  };
+
+  alert(four());
+
+  var upper = sentence.charAt(Math.floor(sentence.length / 2))+third();
+  var k = upper.toUpperCase();
+  alert(k);
+
+
+  function reverseString(upper){
+    var splitString = upper.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray;
+  };
+
+  alert(reverseString(upper));
+
+
 //  alert(firstLetter.toUpperCase());
 //  alert(lastLetter.toUpperCase());
 // //
