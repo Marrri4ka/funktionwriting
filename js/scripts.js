3574499914f6ea0dd363933081abd14e5caead2f
Brendan Hellar and Mariia Stashuk <brendan@>
2019-03-25 13:21:41 -0700

    create a function that will take the first and last letter of the sentence, capitalize them, and return this as a new string

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 
 
 var sentence = prompt("Enter the sentence:");
-var firstLetter = sentence.charAt(0);
-var lastLetter = sentence.charAt(sentence.length-1);
+var result = function(){
+  return sentence.charAt(0).toUpperCase() + sentence.charAt(sentence.length-1).toUpperCase();
 
- alert(firstLetter.toUpperCase());
- alert(lastLetter.toUpperCase());
-
-var upfirst = firstLetter.toUpperCase();
-var uplast = lastLetter.toUpperCase();
-
-var combine = function (upfirst, uplast){
-  return upfirst+ uplast;
 };
- alert(combine(upfirst,uplast));
+alert(result());
+
+
+var reverse = function result(){
+  return sentence.charAt(sentence.length-1).toUpperCase() + sentence.charAt(0).toUpperCase();
+};
+
+alert(reverse());
+//  alert(firstLetter.toUpperCase());
+//  alert(lastLetter.toUpperCase());
+// //
+// // var upfirst = firstLetter.toUpperCase();
+// // var uplast = lastLetter.toUpperCase();
+//
+// var combine = function (upfirst, uplast){
+//   return upfirst+ uplast;
+// };
+//  alert(combine(upfirst,uplast));
+//
+// // function reverseString(combine){
+// //   return combine;
+// // }
+// //
+// // reverseString (combine);
+//   alert(combine(uplast,upfirst));
